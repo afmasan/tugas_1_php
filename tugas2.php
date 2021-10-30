8f7346f16b1da43c4b1238cff4d9214e26e03d0f
afrizal <afrizalmahendra@hotmail.com>
2021-10-30 20:01:47 +0700

    tugas 2

---
+++ tugas2.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 36;
+    $b = 12;
+    $c = 24;
+    $x;
+    $x=($a*$b)/$c;
+echo "36 Hari = 12 Pekerja<br>X hari = 24 Pekerja<br> X=...? <br>Jawaban: $x";
+
+?>
---
+++ tugas2_2.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 80000;
+    $b = 50000;
+    $x;
+    
+    $x=$a-$b;
+echo "Jual = $a<br>Beli = $b <br> Keuntungan=...? <br>Jawaban: $x";
+
+?>
