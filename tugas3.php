0fbc950996e845a9dda43fd7dbe925bfd2489940
afrizal <afrizalmahendra@hotmail.com>
2021-10-30 20:16:21 +0700

    pengumpulan tugas 3

---
+++ tugas3.php
@@ -0,0 +1,13 @@
+<?php
+    $a=360;
+    $b=90;
+    $c=60;
+    $x;
+
+    $x=$a/$b*$c;
+    echo "Jarak Kota A-Z = $a Km<br>
+    Kecepatan Motor =$b km/jam<br>
+    Waktu tempuh =?<br>
+    Jawaban:$x Menit";
+
+?>
---
+++ tugas3_2.php
@@ -0,0 +1,12 @@
+<?php
+    $a=150000;
+    $b=12.5;
+    $x;
+
+    $x=($a*$b/100)+$a;
+    echo "Tabungan Awal = Rp 150000<br>
+    Bunga Setahun = 12.5%<br>
+    Total Tabungan Setahun=?<br>
+    Jawaban:Rp. $x ";
+
+?>
