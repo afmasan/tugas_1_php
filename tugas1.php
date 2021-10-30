eb47fede4415872a7ee2be007639268d43e7841d
afrizal <afrizalmahendra@hotmail.com>
2021-10-30 17:53:05 +0700

    Tugas 1 PHP
    Tugas 1 untuk niomic

---
+++ tugas1.php
@@ -0,0 +1,60 @@
+<?php
+
+$a_goreng = 13000;
+$a_bakar = 15000;
+$a_sayur = 13000;
+$tempe = 1000;
+$tahu = 1000;
+$soto = 3000;
+$nasi = 5000;
+$teh = 4000;
+$tebu = 5000;
+
+echo "<h1> DAFTAR MENU</h1><table>
+<tr>
+    <td><li></td>
+    <td>Ayam Goreng</td>
+    <td>= Rp. $a_goreng</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Ayam Bakar</td>
+    <td>= Rp. $a_bakar</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Ayam Sayur</td>
+    <td>= Rp. $a_sayur</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Tempe Goreng</td>
+    <td>= Rp. $tempe</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Tahu Goreng</td>
+    <td>= Rp. $tahu</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Soto</td>
+    <td>= Rp. $soto</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Nasi Putih</td>
+    <td>= Rp. $nasi</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Es Teh</td>
+    <td>= Rp. $teh</li></td>
+</tr>
+<tr>
+    <td><li></td>
+    <td>Es Tebu</td>
+    <td>= Rp. $tebu</li></td>
+</tr>
+</table>";
+?>
